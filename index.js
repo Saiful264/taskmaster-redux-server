8fae449efaf9f19c015aeba8d908c7fe6b803e9d
Saiful264 <saifulislamx264@gmail.com>
2023-10-02 21:08:26 +0600

    backend counction finally completed

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 
-// const uri = process.env.DATABASE_URI;
-const uri =
-  "mongodb+srv://TechTitans:fH7QVbLjb9BcE6Sr@techtitans.gvuoct6.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = process.env.DATABASE_URI;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.
+// DB_PASS}@cluster0.qawsvmr.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
